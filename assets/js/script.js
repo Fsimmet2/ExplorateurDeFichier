c03501ddbc2435bb0ac374c9e4899a64bd1dd8e9
Fsimmet2 <floriane.s@codeur.online>
2017-11-07 15:17:25 +0100

    ça marche

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,71 @@
 $(document).ready(function(){
 
-
-    $.ajax({
-        url: "explo.php",
-        type : 'post',
-        dataType: 'json', 
-        success: function(result){
-            
-                $.each( result, function( i, val ) {
-                    console.log(val.name);
-                    console.log(i);
-                    if(val.type == 'dir'){
-                        console.log('dir');
-                        $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/sacclosed.png' width='110' height='145' ></br>" + val.name);
-                    }
-                    else if(val.extension == 'php'){
-                        console.log('php');
-                        $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/php.png' width='110' height='145' ></br>" + val.name);
-                    }
-                    else if(val.extension == 'html'){
-                        console.log('html');
-                        $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/HTML.png' width='110' height='145' ></br>" + val.name);
-                    }
-                    else if(val.extension == 'css'){
-                        console.log('css');
-                        $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/css.png' width='110' height='145' ></br>" + val.name);
-                    }
-                    else if(val.extension == 'png'){
-                        console.log('css');
-                        $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/png.png' width='110' height='145' ></br>" + val.name);
-                    }
-                    else
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/sacopen.png' width='110' height='145' ></br>" + val.name);
-                   });
-                
-            
-            
-
-        },
-        error: function(){
-            console.log('error');
-        },
-    });
+    generate();
 });
 
+function generate(path = ''){
+$.ajax({
+    url: "http://localhost/ExplorateurDeFichier/assets/php/explo.php",
+    type : 'post',
+    dataType: 'json',
+    data: {toto : path},
+    success: function(result){
+        console.log(result);
+            $.each( result, function( i, val ) {
+                if(val.type == 'dir'){
+                    console.log('dir');
+                    $('#affichage').append("<div data="+val.path+" class='block folder text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/sacclosed.png' width='110' height='145' ></br>" + val.name);
+                }
+                else if(val.extension == 'php'){
+                    console.log('php');
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/php.png' width='110' height='145' ></br>" + val.name);
+                }
+                else if(val.extension == 'html'){
+                    console.log('html');
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/HTML.png' width='110' height='145' ></br>" + val.name);
+                }
+                else if(val.extension == 'css'){
+                    console.log('css');
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/css.png' width='110' height='145' ></br>" + val.name);
+                }
+                else if(val.extension == 'js'){
+                    console.log('js');
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/js.png' width='110' height='145' ></br>" + val.name);
+                }
+                else if(val.extension == 'png'){
+                    console.log('png');
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/png.png' width='110' height='145' ></br>" + val.name);
+                }
+                else if(val.extension == 'jpg' || val.extension == 'jpeg'){
+                    console.log('jpg');
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/jpg.png' width='110' height='145' ></br>" + val.name);
+                }
+                else if(val.extension == 'svg'){
+                    console.log('svg');
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/svg.png' width='110' height='145' ></br>" + val.name);
+                }
+                else if(val.extension == 'gif'){
+                    console.log('gif');
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/gif.png' width='110' height='145' ></br>" + val.name);
+                }
+                else
+                $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/sacopen.png' width='110' height='145' ></br>" + val.name);
+               });
+            
+            $( ".folder" ).click(function() {
+
+                var path = $(this).attr("data");
+                $('#affichage').html("");
+                generate(path);
+
+            });
+        
+
+    },
+    error: function(){
+        console.log('error');
+        
+        
+    },
+});
+}
