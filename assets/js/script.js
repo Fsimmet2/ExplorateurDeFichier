52b330c59860b6d4b06942f478fda18043239578
Fsimmet2 <floriane.s@codeur.online>
2017-11-08 09:32:58 +0100

    animation et opacité renard

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,49 +15,49 @@ $.ajax({
                 
                 if(val.type == 'dir'){
                     if(val.name[0] == '.'){
-                        $('#affichage').append("<div class='cacher folder block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/creature.jpg' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
+                        $('#affichage').append("<div class=' roll-in-right folder block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/creature.png' width='100%' ></br>" + val.name + '<br>' + val.size + ' octets');
                     }
-                    console.log('dir');
-                    $('#affichage').append("<div data="+val.path+" class='block folder text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/sacclosed.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
+                    else
+                        $('#affichage').append("<div data="+val.path+" class='block roll-in-right folder text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/sacclosed.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
                 }
                 else if(val.extension == 'php'){
                     console.log('php');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/php.png' width='110' height='145' ></br>" +val.name + '<br>' + val.size + ' octets');
+                    $('#affichage').append("<div class='block roll-in-right text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/php.png' width='110' height='145' ></br>" +val.name + '<br>' + val.size + ' octets');
                 }
                 else if(val.extension == 'html'){
                     console.log('html');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/HTML.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
+                    $('#affichage').append("<div class='block roll-in-right text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/HTML.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
                 }
                 else if(val.extension == 'css'){
                     console.log('css');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/css.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
+                    $('#affichage').append("<div class='block roll-in-right text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/css.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
                 }
                 else if(val.extension == 'js'){
                     console.log('js');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/js.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
+                    $('#affichage').append("<div class='block roll-in-right text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/js.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
                 }
                 else if(val.extension == 'png'){
                     console.log('png');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/png.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
+                    $('#affichage').append("<div class='block roll-in-right text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/png.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
                 }
                 else if(val.extension == 'jpg' || val.extension == 'jpeg'){
                     console.log('jpg');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/jpg.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
+                    $('#affichage').append("<div class='block roll-in-right text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/jpg.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
                 }
                 else if(val.extension == 'svg'){
                     console.log('svg');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/svg.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
+                    $('#affichage').append("<div class='block roll-in-right text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/svg.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
                 }
                 else if(val.extension == 'gif'){
                     console.log('gif');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/gif.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
+                    $('#affichage').append("<div class='block roll-in-right text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/gif.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
                 }
                 
 
                
                 
                 else
-                $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/sacopen.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
+                $('#affichage').append("<div class='block roll-in-right text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/sacopen.png' width='110' height='145' ></br>" + val.name + '<br>' + val.size + ' octets');
                });
             
             $( ".folder" ).click(function() {
