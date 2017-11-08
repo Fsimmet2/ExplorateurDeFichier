741a475b890e126a7305c28bcc5cc78b8742b356
Fsimmet2 <floriane.s@codeur.online>
2017-11-08 19:15:35 +0100

    breadcrumbs

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,15 +58,22 @@ $.ajax({
     dataType: 'json',
     data: {toto : path},
     success: function(result){
-       console.log(result[0].path);
-		
+       
+       $('#breadcrumbs').html(""); 
+
+       //aller rechercher le chemin
+       var a = result[0].path;
+       var b = a.replace(/\\/g, "/");
+       var arr = b.split('/');
+       arr.pop();
+       
+       $('#breadcrumbs').append("<div class='barre text-center col-xs-12 col-sm-12 col-md-12 col-lg-12'> Tu es ici : "+ arr.join('/') +"</div>");
+
 		var previous = rmv(result[0].path);
 		
-		console.log('new   ' + previous );
 		
 		if(!(result[0].dirpath == '/var/www/html/ExplorateurDeFichier')){
 		$('#affichage').append("<div  class='block roll-in-left fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/fleche.png' class='fleche img-responsive ' data-p='"+previous+"' alt='retour' width='120' height='120' ></br>RETOUR" );
-			console.log("BONJOUR       " +previous);
 		}
 		
             $.each( result, function( i, val ) {
@@ -77,42 +84,34 @@ $.ajax({
                         $('#affichage').append("<div class=' hidden'></div>");
                     }
                     else if(val.name[0] == '.'){
-                        $('#affichage').append("<div data="+val.path+" class=' roll-in-right fichier block text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/creature.png' alt='dossier' width='100%' ></br>" + val.name + '<br>' + parseInt(val.size/8) + ' bits');
+                        $('#affichage').append("<div data="+val.path+" class=' roll-in-right fichier block text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/creature.png' alt='dossier' width='100%' ></br>" + val.name + '<br>' + humanFileSize(val.size) + '</div>');
                     }
                     else
-                        $('#affichage').append("<div data="+val.path+" class='block roll-in-right folder text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/sacclosed.png' alt='dossier' width='100%' ></br>" + val.name + '<br>' + parseInt(val.size/8) + ' bits ');
+                        $('#affichage').append("<div data="+val.path+" class='block roll-in-right folder text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/sacclosed.png' alt='dossier' width='100%' ></br>" + val.name + '<br>' + humanFileSize(val.size) + '</div>');
                 }
                 else if(val.extension == 'php'){
-                    console.log('php');
-                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/php.png' alt='fichier' width='100%' ></br>" +val.name + '<br>' + parseInt(val.size/8) + " bits <br> <button class='btn btn-default' onclick='dl(1)'>Download</button> </div>" );
+                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/php.png' alt='fichier' width='100%' ></br>" +val.name + '<br>' + humanFileSize(val.size) + " <br> <button class='btn btn-default' onclick='dl(1)'>Download</button> </div>" );
                 }
                 else if(val.extension == 'html'){
-                    console.log('html');
                     $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/HTML.png' width='100%'  alt='fichier' ></br>" + val.name + '<br>' + parseInt(val.size/8) + " bits <br> <button class='btn btn-default' onclick='dl(0)'>Download</button> </div>");
                 }
                 else if(val.extension == 'css'){
-                    console.log('css');
-                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/css.png' width='100%'  alt='fichier'></br>" + val.name + '<br>' + parseInt(val.size/8) + " bits <br> </div>");
+                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/css.png' width='100%'  alt='fichier'></br>" + val.name + '<br>' + humanFileSize(val.size + '</div>'));
                 }
                 else if(val.extension == 'js'){
-                    console.log('js');
-                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/js.png' width='100%'  alt='fichier'></br>" + val.name + '<br>' + parseInt(val.size/8) + " bits <br> </div>");
+                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/js.png' width='100%'  alt='fichier'></br>" + val.name + '<br>' + humanFileSize(val.size) + '</div>');
                 }
                 else if(val.extension == 'png'){
-                    console.log('png');
-                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/png.png' width='100%'  alt='fichier'></br>" + val.name + '<br>' + parseInt(val.size/8) + " bits <br> </div>");
+                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/png.png' width='100%'  alt='fichier'></br>" + val.name + '<br>' + humanFileSize(val.size) + '</div>');
                 }
                 else if(val.extension == 'jpg' || val.extension == 'jpeg'){
-                    console.log('jpg');
-                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/jpg.png' width='100%'  alt='fichier'></br>" + val.name + '<br>' + parseInt(val.size/8) + " bits <br> </div>");
+                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/jpg.png' width='100%'  alt='fichier'></br>" + val.name + '<br>' + humanFileSize(val.size) + '</div>');
                 }
                 else if(val.extension == 'svg'){
-                    console.log('svg');
-                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/svg.png' width='100%'  alt='fichier'></br>" + val.name + '<br>' + parseInt(val.size/8) + " bits <br> </div>");
+                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/svg.png' width='100%'  alt='fichier'></br>" + val.name + '<br>' + humanFileSize(val.size) + '</div>');
                 }
                 else if(val.extension == 'gif'){
-                    console.log('gif');
-                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/gif.png' width='100%'  alt='fichier'></br>" + val.name + '<br>' + parseInt(val.size/8) + " bits <br> </div> ");
+                    $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/gif.png' width='100%'  alt='fichier'></br>" + val.name + '<br>' + humanFileSize(val.size) + '</div>');
 
                 }
                 
@@ -120,15 +119,13 @@ $.ajax({
                 
                 else
 
-                $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/fichier.png' width='100%'  ></br>" + val.name + '<br>' + parseInt(val.size/8) + " bits <br> </div>" );
+                $('#affichage').append("<div class='block roll-in-right fichier text-center col-xs-6 col-sm-3 col-md-2 col-lg-2'><img src='assets/img/fichier.png' width='100%'  ></br>" + val.name + '<br>' + humanFileSize(val.size) + '</div>');
 
                });
             
 			
             $( ".fleche" ).click(function() {
-				console.log($(this));
                	var pathPrevious = $(this).attr("data-p");
-				console.log(pathPrevious);
 				$('#affichage').html("");
 				generate(pathPrevious);
                 	
@@ -155,3 +152,10 @@ $.ajax({
     },
 });
 }
+
+function humanFileSize(size) {
+    var i = size == 0 ? 0 :Math.floor( Math.log(size) / Math.log(1024) );
+    return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+};
+
+
