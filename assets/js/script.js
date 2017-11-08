99c07c734c8eac29efa7e28783e420d4091c43a8
Yohann <yohann.t@codeur.online>
2017-11-08 09:34:23 +0100

    fleche qui marche

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,60 +3,119 @@ $(document).ready(function(){
     generate();
 });
 
+function rmv(the_url)
+{
+	if ((the_url == "/xampp/htdocs") || (the_url == "/xampp/") || (the_url == "/")) {
+		the_url =="/xampp/htdocs/ExplorateurDeFichier"
+		var str = the_url;
+		console.log("A    "+str);
+		var res = str.replace(/\\/g, "/");
+		console.log("B    "+res);
+		var the_arr = res.split('/');
+		console.log("C    "+the_arr);
+		return(the_arr.join('/') );
+	}
+	else{
+		var str = the_url;
+console.log("A    "+str);
+var res = str.replace(/\\/g, "/");
+	console.log("B    "+res);
+var the_arr = res.split('/');
+	console.log("C    "+the_arr);
+the_arr.pop();
+	the_arr.pop();
+return(the_arr.join('/') );
+	}
+	
+
+}
+
+
 function generate(path = ''){
+	
+	
 $.ajax({
-    url: "http://localhost/ExplorateurDeFichier/assets/php/explo.php",
+    url: "http://localhost:8090/ExplorateurDeFichier/assets/php/explo.php",
     type : 'post',
     dataType: 'json',
     data: {toto : path},
     success: function(result){
-        console.log(result);
+       console.log(result[0].path);
+		
+		var previous = rmv(result[0].path);
+		
+		console.log('new   ' + previous );
+		
+		if(!(result[0].dirpath == '/xampp/htdocs/ExplorateurDeFichier')){
+		$('#affichage').append("<div  class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/fleche.png' class='fleche' data-p='"+previous+"' alt='retour' width='120' height='120' ></br>RETOUR" );
+			console.log("BONJOUR       " +previous);
+		}
+		
             $.each( result, function( i, val ) {
+				
                 if(val.type == 'dir'){
-                    console.log('dir');
-                    $('#affichage').append("<div data="+val.path+" class='block folder text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/sacclosed.png' width='110' height='145' ></br>" + val.name);
+                    
+                    $('#affichage').append("<div data="+val.path+" class='block folder text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/sacclosed.png' alt='dossier' width='120' height='120' ></br>" + val.name);
                 }
                 else if(val.extension == 'php'){
-                    console.log('php');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/php.png' width='110' height='145' ></br>" + val.name);
+                    
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/php.png' alt='fichier php' width='120' height='120' ></br>" + val.name);
                 }
                 else if(val.extension == 'html'){
-                    console.log('html');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/HTML.png' width='110' height='145' ></br>" + val.name);
+                    
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/HTML.png' alt='fichier html' width='120' height='120' ></br>" + val.name);
                 }
                 else if(val.extension == 'css'){
-                    console.log('css');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/css.png' width='110' height='145' ></br>" + val.name);
+                   
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/css.png' alt='fichier css' width='120' height='120' ></br>" + val.name);
                 }
                 else if(val.extension == 'js'){
-                    console.log('js');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/js.png' width='110' height='145' ></br>" + val.name);
+                    
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/javascript.png' alt='fichier javascript' width='100%' ></br>" + val.name);
                 }
                 else if(val.extension == 'png'){
-                    console.log('png');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/png.png' width='110' height='145' ></br>" + val.name);
+                    
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/png.png' alt='image png'width='120' height='120' ></br>" + val.name);
                 }
                 else if(val.extension == 'jpg' || val.extension == 'jpeg'){
-                    console.log('jpg');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/jpg.png' width='110' height='145' ></br>" + val.name);
+                    
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/jpg.png' alt='fichier jpg' width='120' height='120' ></br>" + val.name);
                 }
                 else if(val.extension == 'svg'){
-                    console.log('svg');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/svg.png' width='110' height='145' ></br>" + val.name);
+                    
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/svg.png' alt='fichier svg' width='120' height='120' ></br>" + val.name);
                 }
                 else if(val.extension == 'gif'){
-                    console.log('gif');
-                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/gif.png' width='110' height='145' ></br>" + val.name);
+                    
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/gif.png' alt='fichier gif' width='120' height='120' ></br>" + val.name);
+                }
+				else if((val.extension == 'md') || (val.extension == 'txt') || (val.extension == 'sys') || (val.extension == 'psd') || (val.extension == 'ai') || (val.extension == 'xls') || (val.extension == 'doc') || (val.extension == 'docx')){
+                    
+                    $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/gif.png' alt='fichier gif' width='120' height='120' ></br>" + val.name);
                 }
                 else
-                $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/sacopen.png' width='110' height='145' ></br>" + val.name);
+                $('#affichage').append("<div class='block text-center col-xs-6 col-sm-6 col-md-3 col-lg-2'><img src='assets/img/fichier.png' width='120' height='120'' ></br>" + val.name);
                });
             
-            $( ".folder" ).click(function() {
+			
+            $( ".fleche" ).click(function() {
+				console.log($(this));
+               	var pathPrevious = $(this).attr("data-p");
+				console.log(pathPrevious);
+				$('#affichage').html("");
+				generate(pathPrevious);
+                	
 
-                var path = $(this).attr("data");
+            });
+		
+            $( ".folder" ).click(function() {
+				
+                var path = ($(this).attr("data"));
                 $('#affichage').html("");
+				
+				
                 generate(path);
+				
 
             });
         
--- assets/php/explo.php
+++ assets/php/explo.php
@@ -6,28 +6,35 @@ if(isset($_POST['toto'])){
     
     $dir = $_POST['toto'];
     if($dir == ""){
-        $dir = '/var/www/html/ExplorateurDeFichier/';
+        $dir = '/xampp/htdocs/ExplorateurDeFichier/';
+		$pathname = $dir;
     }else{
-        $dir = $_POST['toto']; 
+        $dir = $_POST['toto'];
+		$pathname = $_POST['toto'];
     }
 }else{
-    $dir = '/var/www/html/ExplorateurDeFichier/';
+    $dir = '/xampp/htdocs/ExplorateurDeFichier/';
 }
 
 foreach (new DirectoryIterator($dir) as $file) {
     if($file->isDot()) continue;
     $tab[] = [
-        'name' => $file->getFilename(),
+		
+		'dirpath' => $file->getPath(),
+		'name' => $file->getFilename(),
         'extension' => $file->getExtension(),
         'path' => $file->getPathname(),
         'type' => $file->getType(),
         'size' => $file->getSize()
     ];
+	
+	
     
 }
 
 
 echo json_encode($tab);
+
 
 ?>
 
